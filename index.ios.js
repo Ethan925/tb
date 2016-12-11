a121a7587d8e0c29b3779af47a47a6fa89332d62
Ethan Long <ethan.long@vertical-knowledge.com>
2016-12-11 00:49:55 -0500

    pushing to db

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,65 @@
  */
 
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
+// import ReactNative from 'react-native';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView
 } from 'react-native';
 
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js');
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyATXL8waxCWCSWQRALbpG39yBA4WiIsKy4",
+  authDomain: "<your-auth-domain>",
+  databaseURL: "https://talentbase-b2a57.firebaseio.com/",
+  storageBucket: "gs://talentbase-b2a57.appspot.com",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const provider = new firebase.auth.FacebookAuthProvider();
+
+
 export default class tb extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = firebaseApp.database().ref();
+  }
+
+  componentDidMount() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
+    })
+    console.log('did mount', this.itemsRef);
+    this.itemsRef.push({ title: 'heyyyyyy' })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! talent
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+
+        <StatusBar title="Fuck bitches get money" />
+
       </View>
     );
   }
-}
+  _renderItem(item) {
+    return (
+      <ListItem item="{item}" />
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}
 
 AppRegistry.registerComponent('tb', () => tb);
